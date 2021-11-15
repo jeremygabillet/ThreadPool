384eaadc96d470228aa0df888dbf0a1834e3428a
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-15 14:47:31 +0100

    Finishing quest

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
 
-namespace ThreadPool
+namespace ThreadPoolQuest
 {
     class Program
     {
+        private CountdownEvent _countdownEvent;
+        private int _threadsCount;
+        private ConcurrentStack<int> _concurrentStack;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program(5);
+            program.Run(1000000);
+        }
+        public Program(int threadsCount)
+        {
+            _threadsCount = threadsCount;
+            _countdownEvent = new CountdownEvent(threadsCount);
+            _concurrentStack = new ConcurrentStack<int>();
+        }
+        public void Run(int numIntegers)
+        {
+            Random random = new Random();
+            for (int i = 0; i < numIntegers/_threadsCount; i++)
+            {
+                for (int j = 0; j < _threadsCount; j++)
+                {
+                    ThreadPool.QueueUserWorkItem(x =>
+                    {
+                        _concurrentStack.Push(random.Next(0, 1000000));
+                        _countdownEvent.Signal();
+                    });
+                }
+                _countdownEvent.Wait();
+                _countdownEvent.Reset(_threadsCount);
+            }
+            System.Console.WriteLine($"Concurrent stack reached {_concurrentStack.Count} integers.");
         }
     }
 }
